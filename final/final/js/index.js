cd6d74b762e57cc075a3fd6efab539a0ab46bbd4
Mronden <1398349398@qq.com>
2019-08-01 17:56:05 +0800

    first commit

---
+++ final/final/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ASUS on 2019/7/30.
+ */
+var lab11 = document.getElementsByClassName('lab1')[0];
+var lab22 = document.getElementsByClassName('lab2')[0];
+lab22.onclick = function(){
+    lab11.style.backgroundColor = "#7295C6";
+    lab11.style.color = 'white';
+}
